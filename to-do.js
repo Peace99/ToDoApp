0270fe87935f500883521b4deafad8bf08773489
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2021-07-28 13:10:22 +0100

    delete

--- to-do.js
+++ to-do.js
@@ -1,38 +1,36 @@
-let input = document.querySelector(".element input")
-let btn = document.querySelector("#Add")
-let list = document.querySelector("#list")
-let element = document.getElementsByTagName('li')
+
+let btn = document.querySelector("#Add");
+let list = document.querySelector("#list");
+let element = document.getElementsByTagName("li");
 
 btn.addEventListener("click", () => {
-    let text = input.value 
-    //if you enter an empty string, it alerts you to write something
-    if (text === "") {
-        alert('you must write something')
-    }
-    //
-    else{
-        let li = document.createElement("li")
-        li.innerHTML = text
-        list.insertBefore(li, list.childNodes[0])
-        list.value += ""
-    }
-    
-    let del = document.querySelectorAll(".delBtn")
-    for (var i = 0; i < del.length; i++) {
-    del[i].addEventListener("click", function(){
-        list.removeChild(list.childNodes[0])
-    })
 
-    document.querySelector("#todo").value = ""
-}
-})
+let input = document.querySelector(".element input");
+let text = input.value;
+input.value="";
 
+  //if you enter an empty string, it alerts you to write something
+  if (text === "") {
+    alert("you must write something");
+  }
+  //
+  else {
+    let li = document.createElement("li");
+    li.innerHTML =
+     `<span>${text}</span>`+ " " + `<button class="delBtn">DEL</button>`;
+   
+    list.insertBefore(li, list.childNodes[0]);
+    del = document.querySelectorAll(".delBtn");
+    del[0].addEventListener("click", function () {
+        li.parentNode.removeChild(li);
+    });
+  }
+  
 
+});
 
-list.addEventListener("click", function(e) {
-    if(e.target.tagName == 'LI')
-    {
-        e.target.classList.toggle('checked')
-    }
-})
-
+list.addEventListener("click", function (e) {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+  }
+});
